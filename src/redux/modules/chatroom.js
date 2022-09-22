eac09aba85122b1a9e0845154386532982d4e2ac
limdaeun1 <limdaeun1_@naver.com>
2022-09-22 14:21:31 +0900

    Feat: "추가 리스트불러오기, 방생성하기" 리스트페이지 방리스트 get 구현 방 생성하기 post 구현

---
+++ src/redux/modules/chatroom.js
@@ -0,0 +1,60 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { instance } from "../../shared/api";
+
+
+//채팅방 목록 가져오기
+export const getChatrooms = createAsyncThunk(
+    "chatroom/getChatrooms",
+    async (_, thunkAPI) => {
+      try {
+        const data = await instance.get("/auth/chat/rooms");
+        return data.data.data;
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+      }
+    }
+  );
+
+
+//방 생성하기  
+export const createRoom = createAsyncThunk(
+"chatroom/createRoom",
+async (payload, thunkApI) => {
+    try {
+    const data = await instance.post("/auth/chat/room" , payload);
+    window.alert("방을 생성하였습니다");
+    window.location.assign("/list"); 
+    return data
+    } catch (error) {
+    return thunkApI.rejectWithValue(error);
+    }
+}
+);
+
+
+const initialState = {
+  roomlist:[],
+  };
+
+export const chatroom = createSlice({
+    name: "chatroom",
+    initialState,
+    reducers: {},
+
+    extraReducers: {
+        [getChatrooms.pending]: (state) => {
+          state.isLoading = true; 
+        },
+        [getChatrooms.fulfilled]: (state, action) => {
+          state.isLoading = false; 
+          state.roomlist = action.payload;    
+        },
+        [getChatrooms.rejected]: (state, action) => {
+          state.isLoading = false; 
+          state.error = action.payload; 
+        },
+    }
+  });
+
+
+export default chatroom.reducer;
