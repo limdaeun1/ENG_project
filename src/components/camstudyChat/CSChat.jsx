8b766369982f2bdcae7347c8d09a6b2caca0e176
yesol_lee <thfl3363@hanmail.net>
2022-10-01 18:59:08 +0900

    Design "채팅 디자이 수정"
    스크롤바, 메세지창 디자인 수정

--- src/components/camstudyChat/CSChat.jsx
+++ src/components/camstudyChat/CSChat.jsx
@@ -14,17 +14,18 @@ const CSChat = () => {
   const roomId = useParams();
   const client = useRef({});
   const [chat, setChat] = useState({content:""})
-
   const [messages, setMessages] = useState([{ 
   chatMessage: "", 
   user: "",
   type:"",                                                      //백과 협의한 메세지 type(0:입장, 1:퇴장, 2:채팅)
   image:"" }]);
+  const [notice, setNoitce] = useState(false)
   const inputRef = useRef("");
+  const noticeRef = useRef("");
   const navigate = useNavigate();
 
   const chattingRef = useRef(null);
-  const scrollToElement = () => chattingRef.current.scrollIntoView({behavior:"smooth"});
+  const scrollToElement = () => chattingRef.current?.scrollIntoView({behavior:"smooth"});
 
   
   
@@ -95,7 +96,7 @@ const CSChat = () => {
         ..._messages,
         { chatMessage: content.msg, user: content.sender, type: content.type,image:content.image },
       ]);
-      setTimeout(()=>scrollToElement(),50);
+      setTimeout(()=>scrollToElement(),30);
     });
   };
 
@@ -117,22 +118,34 @@ const CSChat = () => {
       }),
     }); 
     setChat({content:""});
-    // setTimeout(()=>scrollToElement(),200);
-   
     };
     
     
   };
 
   client.current.onStompError = function (frame) {
-    // Will be invoked in case of error encountered at Broker
-    // Bad login/passcode typically will cause an error
-    // Complaint brokers will set `message` header with a brief message. Body may contain details.
-    // Compliant brokers will terminate the connection after any error
     console.log("Broker reported error: " + frame.headers["message"]);
     console.log("Additional details: " + frame.body);
   };
 
+    //공지 등록
+    const onSubmitNotice = () =>{
+      if (noticeRef.current.value == ""){
+        alert("공지사항을 입력하세요")
+    } else{
+            client.current.publish({
+        destination: "/pub/chat/message",
+        headers: { Authorization: Authorization },
+        //전송할 데이터를 입력
+        body: JSON.stringify({
+          type: 2,
+          message: "!공지등록 "+noticeRef.current.value,
+          roomId: roomId.id,
+        }),
+      })
+    }
+
+    } 
   //연결 중단
   const disconnect = () => {
 
@@ -156,8 +169,11 @@ navigate("/list");
 
   };
 
+  //엔터키 제어
   const handleKeyPress = e => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter' && e.shiftKey) { // [shift] + [Enter] 치면 리턴
+      return;}
+   else if(e.key === 'Enter') {             //[Enter]치면 전송
       submit();
     }
   }
@@ -172,53 +188,90 @@ navigate("/list");
   }
   // console.log(chat) 
 
+
+//공지등록상태 열고닫기
+const changeNotice = () => {
+  setNoitce(!notice);
+  setChat({content:""})
+}
+
+
   return (
-    <>
-    <button onClick={()=>{navigate("/")}}>홈으로</button>
-  <div>
+    <div style={{display:"flex"}}>
+    <div>
+      <div>
           <ChatBox id="chatBox">
-           { messages.map((c, i) => {
+         { messages.map((c, i) => {
           return (
-             c.type === 2 ?
+            c.type === 2 ?
             (c.user == name ?
-            <Chat2 key={i}> 
-              <What>{c.chatMessage}</What>
-             <div ref={chattingRef}/>
-            </Chat2>:
-            <Chat key={i}>
-              <img width={50} src={c.image}/>
-              <Who>{c.user}</Who>
-              <What>{c.chatMessage}</What>
+
+            <MyChat key={i}>
+              <MyName>{c.user}</MyName>
+              <MyMsg>{c.chatMessage}</MyMsg>
               <div ref={chattingRef}/>
-            </Chat>)
-            :
-            <Chat key={i}>
-            <p>{c.chatMessage}</p>
-            <div ref={chattingRef}/>
-          </Chat>
+            </MyChat>:
+
+            <OtherChat key={i}>
+              <ImgBox src={c.image}/>
+              <div style={{alignItems:"center"}}>
+              <OtherName>{c.user}</OtherName>
+              <OtherMsg>{c.chatMessage}</OtherMsg>
+              </div>
+              <div ref={chattingRef}/>
+            </OtherChat>)
+            :(c.type === 3? 
+              <OtherChat key={i}>
+                <div>
+                   <p>{c.chatMessage}</p>
+              <div ref={chattingRef}/>
+                </div>
+             
+            </OtherChat>:
+            <OtherChat key={i}>
+              <p>{c.chatMessage}</p>
+              <div ref={chattingRef}/>
+            </OtherChat>)
+
+
           );
         })}
-        
       </ChatBox>
       
   </div>
 
-      
+  {
+        notice === false ?
 
-      <SendBox>
-        <InputBox
-        name="content"
-        value={chat.content}
-        ref={inputRef}
-        onKeyPress={handleKeyPress}
-        onChange = {changeHandler}
-        ></InputBox>
-        <SendBut
-          onClick={() => {submit()}}>
-          전송
-        </SendBut>
-      </SendBox>
-    </>
+        <SendBox>
+        <p>채팅모드</p>
+        <InputBox 
+        name="content" 
+        value={chat.content} 
+        ref={inputRef} 
+        onKeyUp={handleKeyPress}             //keydown or keypress일때하면 안됨. 올라갈때 실행되야지 엔터가 자동으로 안먹힘. 그래서 keyup사용
+        onChange = {changeHandler}/>
+        <SendBut onClick={() => {submit()}}>전송</SendBut>
+          <SendBut onClick={()=>{changeNotice()}}>공지 모드</SendBut> 
+        </SendBox>
+
+        :
+
+          <SendBox>
+            <p>공지모드</p>
+            <NoticeInputBox ref={noticeRef} onKeyUp={handleKeyPress} />
+            <NoticeBut onClick={()=>{onSubmitNotice();changeNotice()}}>등록</NoticeBut>
+            <NoticeBut onClick={()=>{changeNotice()}}>채팅 모드</NoticeBut>
+          </SendBox>
+        
+      }
+    </div>
+
+    <UserListBox>
+      참가자 목록 관리
+    </UserListBox>
+  
+    </div>
   );
 };
 
@@ -235,97 +288,177 @@ const ChatBox = styled.div`
   margin-top: 10px;
   margin-left: 25px;
   background: #ebfbee; */
+  padding: 10px;
   overflow-x: hidden;
   height: 240px;
-  width: 100%;
-  border:1px solid black;
+  /* max-width: 600px; */
+   max-width: 600px;
+  /* border:1px solid black; */
   display: block;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    /* background: rgba(0, 0, 0, 0.3); */
+    background: #d3f9d8;
+    border-radius: 6px;
+  }
 `;
 
 
 
 
-const Chat = styled.div`
+const OtherChat = styled.div`
   display: flex;
   border: none;
-  height: 40px;
   width: 1200px;
-  margin-left: 50px;
-  margin-top: 10px;
+  /* margin-left: 50px; */
+  /* margin-top: 10px; */
 `;
 
 
 
-const Chat2 = styled.div`
-  display: flex;
+const MyChat = styled.div`
   float: right;
+  clear: both;
   border: none;
-  height: 40px;
+  height: auto;
   margin-left: 50px;
   margin-top: 10px;
-  clear: both;
+
 `;
 
+const ImgBox =  styled.img`
+  border-radius: 10px;
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+`
+const MyName=styled.div`
+margin-left:auto;
+border: none;
+width: 50px;
+text-align : center;
+font-weight: bold;
+font-size: 13px;
+align-items: center;
+justify-content: center;
+line-height: 30px;
+`;
 
-const Who = styled.div`
+const OtherName=styled.div`
+border: none;
+width: 50px;
+text-align : center;
+font-weight: bold;
+font-size: 13px;
+align-items: center;
+justify-content: center;
+line-height: 30px;
+/* border-radius: 20px;
+background: #8ce99a; */
+`;
+
+const MyMsg = styled.div`
   border: none;
-  width: 100px;
-  text-align: center;
-  font-weight: bold;
-  font-size: small;
-  align-items: center;
-  justify-content: center;
-  line-height: 35px;
-  border-radius: 20px;
-  background: #8ce99a;
-`;
-
-const What = styled.div`
-  border: solid 1px gray;
   width: fit-content;
   margin-left: 20px;
   padding:0px 10px 0px 10px;
   line-height: 35px;
-  border-radius: 20px;
-  /* //내부 스크롤 */
-  /* overflow: scroll;
-overflow-x: hidden; */
   background: white;
   font-size: small;
+  white-space: pre-wrap;
+border-radius: 10px 10px 0px 10px;
+padding: 0px 10px 0px 10px;
+color:white;
+  background-color: #40c057;
+`;
+
+const OtherMsg = styled.div`
+  border: none;
+  width: fit-content;
+  /* margin-left: 20px; */
+  padding:0px 10px 0px 10px;
+  line-height: 35px;
+  background: white;
+  font-size: small;
+  white-space: pre-wrap;
+border-radius: 10px 10px 10px 0px;
+padding: 0px 10px 0px 10px;
+color:white;
+  background-color: #ced4da;
 `;
 
 const SendBox = styled.div`
-  border: none;
+  /* border: none; */
+  border: 1px solid blue;
   margin-top: 20px;
   height: 30px;
-  width: 1200px;
+  max-width: 600px;;
   display: flex;
-  border-radius: 20px;
   align-items: center;
   justify-content: center;
-  margin-left: 50px;
+  /* margin-left: 50px; */
 `;
 
-const InputBox = styled.input`
-  border: solid 1px green;
-  width: fit-content;
-  height: 40px;
-  border-radius: 20px;
+const InputBox=styled.textarea`
+border: solid 1px green ;
+width: 100%;
+height: 30px;
+border: 1px solid #51cf66;
+border-radius: 20px;
+`;
+
+const NoticeInputBox=styled.textarea`
+border: solid 1px green ;
+width: 850px;
+height: 30px;
+border: 1px solid #1c7ed6;
+border-radius: 5px;
 `;
 
 const SendBut = styled.div`
-  width: 150px;
+  width: fit-content;
   border: none;
+  min-width:60px;
   background: #69db7c;
-  height: 40px;
-  border-radius: 20px;
-  margin-left: 50px;
+  height: 30px;
+  border-radius: 5px;
+  margin-left: 15px;
   text-align: center;
   line-height: 30px;
   font-weight: bold;
-  font-size: middle;
-  line-height: 40px;
-  &:hover {
-    background-color: #89f6ab;
-  }
+  font-size: small;
+  line-height: 27px;
+  /* &:hover {
+    background-color: #1c7ed6;
+  } */
 `;
+
+const NoticeBut = styled.div`
+  width: fit-content;
+  border: none;
+  min-width:60px;
+  background: #1c7ed6;
+  height: 30px;
+  border-radius: 5px;
+  margin-left: 15px;
+  text-align: center;
+  line-height: 30px;
+  font-weight: bold;
+  font-size: small;
+  line-height: 27px;
+  /* &:hover {
+    background-color: #89f6ab;
+  } */
+`;
+
+const UserListBox = styled.div`
+  width: 200px;
+  border: 1px solid black;
+  margin-left: 10px;
+`
