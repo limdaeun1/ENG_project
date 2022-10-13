ee4cad21fe67a3d0c3ed3a1aaf564288c9d0d90e
limdaeun1 <limdaeun1_@naver.com>
2022-10-14 02:51:55 +0900

    Feat "추가 캠스터디방 마이크 카메라 제어" 모든 채팅방/구독자 마이크 카메라 제어 버튼 구현 완료

--- src/components/scriptChat/CamBig.jsx
+++ src/components/scriptChat/CamBig.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 import OpenViduVideoComponent from './OvVideo';
 import mute from "../../img/mute.png";
+import muteon from "../../img/muteon.png";
 import videooff from "../../img/videooff.png";
+import videoon from "../../img/videoon.png";
 
 export default class CamBig extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      micOn : true,
+      videoOn : true,
+    };
+  }
 
   getNicknameTag() {
     // Gets the nickName of the user
@@ -14,9 +24,15 @@ export default class CamBig extends Component {
 publishAudio(e) {
   if (this.props.streamManager.stream.audioActive == true) {
      this.props.streamManager.publishAudio(false);
+       this.setState({
+        micOn : false,
+       })
        console.log("음소거함") }
    else {
        this.props.streamManager.publishAudio(true);
+       this.setState({
+        micOn : true,
+       })
        console.log("켜기")
     }
 }
@@ -24,12 +40,20 @@ publishAudio(e) {
 publishVideo() {
   if(this.props.streamManager.stream.videoActive == true) {
   this.props.streamManager.publishVideo(false);
-       console.log("카메라끄기") }
+  this.setState({
+    videoOn : false,
+   })
+    console.log("카메라끄기") }
   else {
       this.props.streamManager.publishVideo(true);
+      this.setState({
+        videoOn : true,
+       })
       console.log("카메라켜기")
   }
 }    
+
+
 
 render() {   
   return (
@@ -40,8 +64,10 @@ render() {
             <OpenViduVideoComponent streamManager={this.props.streamManager}/>
             <Nick>
             <Iconbox>
-              <Mute src={mute} onClick={()=>this.publishAudio()}></Mute>
-              <Videooff  src={videooff} onClick={()=>this.publishVideo()}></Videooff>
+              {this.state.micOn ? 
+              <Mute src={mute} onClick={()=>this.publishAudio()}></Mute> : <Mute2 src={muteon} onClick={()=>this.publishAudio()}></Mute2>}
+              {this.state.videoOn ?
+              <Videooff  src={videooff} onClick={()=>this.publishVideo()}></Videooff> :  <Videooff2  src={videoon} onClick={()=>this.publishVideo()}></Videooff2>}
             </Iconbox>
               <p>{this.getNicknameTag()}</p>
             </Nick>
@@ -83,12 +109,26 @@ const Mute = styled.img`
 width: 20px;
 height: 15px;
 margin-right: 10px;
+cursor: pointer;
+`
+const Mute2 = styled.img`
+width: 19px;
+height: 15px;
+margin-right: 10px;
+cursor: pointer;
 `
 
 const Videooff = styled.img`
+width: 14px;
+height: 14px;
+cursor: pointer;
+`
+const Videooff2 = styled.img`
 width: 15px;
 height: 15px;
+cursor: pointer;
 `
+
 const Iconbox = styled.div`
 height: 20px;
 margin-right: 10px;
