86d43b5d00c7c9280bf36156ed6652f78c631907
Bomi Kim <bxm1kim@gmail.com>
2022-10-19 18:19:34 +0900

    Design: 수정 마이페이지 디자인 Design: 수정 메인페이지 하단 디자인 Design: 수정 채팅 리스트 페이지 배너 변경
    마이페이지 디자인 수정
    메인페이지 하단 디자인 수정
    채팅 리스트 페이지 배너 변경

--- src/components/main/MainAd.jsx
+++ src/components/main/MainAd.jsx
@@ -5,10 +5,10 @@ const MainAd = () => {
   return (
     <Container>
         <Ad1>
-            <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEpiG5GOuuyq6TP_cgfaZhx95v4768Kuo5Sg&usqp=CAU"></img>
+            <img src ="https://ifh.cc/g/YSy0Pz.png"></img>
         </Ad1>
         <Ad2>
-            <img src ="https://www.lens007.com/wp-content/uploads/%EC%B9%9C%EA%B5%AC%EC%B6%94%EC%B2%9C-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EB%B0%B0%EB%84%88-007.jpg"></img>
+            <img src ="https://ifh.cc/g/YSy0Pz.png"></img>
         </Ad2>
     </Container>
   )
@@ -28,11 +28,11 @@ margin-top: 10%;
 
 const Ad1 = styled.div`
     img{
-        width: 90%;
-        min-width: 170px; 
+        width: 83%;
+        min-width: 300px; 
         height: 100%;
         border-radius: 20px;
-        margin: 0% 5% 0% 5%;
+        margin: 0% 2% 0% 13%;
     }
 
 `
@@ -40,10 +40,10 @@ const Ad1 = styled.div`
 const Ad2 = styled.div`
     margin-left: 5%;
     img{
-        width: 90%;
+        width: 83%;
         height: 100%;
-        min-width: 250px; 
+        min-width: 300px; 
         border-radius: 20px;
-        margin: 0% 5% 0% 5%;
+        margin: 0% 15% 0% 2%;
     }
 `
